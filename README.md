ddf1c40329ecdc7a0f192631ce171acf7f78bec5
Ashley Harris <laylen24@gmail.com>
2025-09-18 21:22:29 -0500

    correct all the errors

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # devtools-day1
+
+-this is a fork project to test javascript and debug it
+
+## how to run this
+1.Clone the repository
+```bash
+git clone repo url
+```
+
+2.Move into the folder
+```bash
+cd repo_name
+```
+
+## Git Useful Tips
+1. Create a new branch
+```bash
+git checkout -b new_branch_name
+```
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Error 1:
 let message = 'Welcome to the debugging exercise!';
-console.log(mesage); 
+console.log(message); 
 
 // Error 2: 
-let button = document.querySelector('.actionButton'); 
+// console.log(document);
+let button = document.querySelector('#actionButton'); 
 button.addEventListener('click', eventHandler); 
 
 // Error 3: 
@@ -18,6 +19,8 @@ function displayOutput() {
   output.innerText = 'You clicked the button!';
 }
 
+let localMessage = 'Initial message';
+
 // Error 6: Misusing variable scope
 function updateMessage() {
   let localMessage = 'Updated message';
@@ -26,7 +29,7 @@ updateMessage();
 console.log(localMessage); 
 
 // Error 7:
-document.getElementById('testButton').addEventListener('click', testFunction());
+document.getElementById('actionButton').addEventListener('click', testFunction());
 
 function testFunction() {
   console.log('Test function executed');
@@ -35,15 +38,16 @@ function testFunction() {
 // Error 8:
 let number1 = 10;
 let number2 = 5;
-console.log('Sum is: ' + (number1 - number2));
+console.log('Sum is: ' + (number1 + number2));
 
 // Error 9: 
-let nullVar = null;
+let nullVar = "null";
 console.log(nullVar.length);
+//console.log(typeof nullVar, nullVar);
 
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
@@ -51,33 +55,36 @@ let importantData =
 console.log('importantData');
 
 // Error 12: 
-tryToCallFunction();
+// tryToCallFunction();
 
 // Error 13:
-let user = { namee: 'Alice' };
+let user = { name: 'Alice' };
 console.log(user.name)
 
 // Error 14: 
-let someNumber = 123;
+
+let someNumber = "123";
+
 console.log(someNumber.toUpperCase()); 
 
 // Error 15: 
-let greeting = Hello, world!;
+let greeting = "Hello, world!";
 
 // Error 16: 
-for (let i = 0; i < 5; i++); {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
 // Error 17:
 let five = '5';
-if (five === 5) {
+if (five === '5') {
     console.log('Five is equal to 5'); // This one should be logged
 } else {
     console.log('Five is not equal to 5'); 
-
-// Error 18:
-Array.prototype.push = function() {
+}
+// Error 18: 
+[].push
+Array.prototype.push2 = function() {
     console.log('Array push method is overwritten');
 };
 let numbers = [];
@@ -85,11 +92,11 @@ numbers.push(1);
 console.log(numbers);
 
 // Error 19:
-console.log(x);
 x = 5;
+console.log(x);
+
 
 // Error 20:
 let myString = 'Hello';
-myString.property = 'World';
-console.log(myString.property); 
-
+myString = 'World';
+console.log(myString); 
